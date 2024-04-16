d5cde8cc764d5d29bc2700c58a81f601e0f78e65
Sandesh Khadka <ksandesh283@gmail.com>
2024-04-16 11:29:55 +0545

    minor fixes on post section

--- routes/posts.js
+++ routes/posts.js
@@ -1,51 +1,57 @@
 const express = require('express');
-const Post = require('../models/post');
+const Post = require('../models/post'); // Adjust the path to your Post model as necessary
 
 const router = express.Router();
 
-
-// A sample route in your Express app (backend)
-app.get('/posts', (req, res) => {
-  Post.find({}, (err, posts) => {
-      if (err) {
-          console.log(err);
-          res.status(500).send('An error occurred', err);
-      } else {
-          res.json(posts);
-      }
-  });
-});
-
-
 // Get All Posts
 router.get('/', async (req, res) => {
-  const posts = await Post.find();
-  res.json(posts);
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).send('An error occurred', err);
+  }
 });
 
 // Create New Post
 router.post('/', async (req, res) => {
-  const newPost = new Post(req.body);
-  const savedPost = await newPost.save();
-  res.json(savedPost);
+  try {
+    const newPost = new Post(req.body);
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Get Specific Post
 router.get('/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.json(post);
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 // Delete Post
 router.delete('/:id', async (req, res) => {
-  const result = await Post.findByIdAndDelete(req.params.id);
-  res.json(result);
+  try {
+    const result = await Post.findByIdAndDelete(req.params.id);
+    res.json(result);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 // Update Post
 router.patch('/:id', async (req, res) => {
-  const result = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(result);
+  try {
+    const result = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(result);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = router;
